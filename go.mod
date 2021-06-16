b6f9295925084716e314b20ad3257966493af8ac
Alessio Dionisi <hello@adns.io>
2021-06-16 22:57:33 +0200

    feat: implement mkcloudinit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/adnsio/mkcloudinit
+
+go 1.16
+
+require (
+	github.com/diskfs/go-diskfs v1.2.0
+	github.com/frankban/quicktest v1.13.0 // indirect
+	github.com/spf13/cobra v1.1.3
+)
---
+++ main.go
@@ -0,0 +1,164 @@
+// Build an iso9660 cloud-init image in seconds
+// Copyright (C) 2021 Alessio Dionisi <hello@adns.io>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/diskfs/go-diskfs"
+	"github.com/diskfs/go-diskfs/disk"
+	"github.com/diskfs/go-diskfs/filesystem"
+	"github.com/diskfs/go-diskfs/filesystem/iso9660"
+	"github.com/spf13/cobra"
+)
+
+type commandOptions struct {
+	metaData      string
+	networkConfig string
+	output        string
+	userData      string
+}
+
+func main() {
+	if err := newCommand().Execute(); err != nil {
+		os.Exit(1)
+	}
+}
+
+func newCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "mkcloudinit",
+		Short: "Build an iso9660 cloud-init image in seconds",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			output, err := cmd.Flags().GetString("output")
+			if err != nil {
+				return err
+			}
+
+			userData, err := cmd.Flags().GetString("user-data")
+			if err != nil {
+				return err
+			}
+
+			metaData, err := cmd.Flags().GetString("meta-data")
+			if err != nil {
+				return err
+			}
+
+			networkConfig, err := cmd.Flags().GetString("network-config")
+			if err != nil {
+				return err
+			}
+
+			opts := &commandOptions{
+				metaData:      metaData,
+				networkConfig: networkConfig,
+				output:        output,
+				userData:      userData,
+			}
+
+			if err := runCommand(opts); err != nil {
+				fmt.Printf("%s", err)
+				os.Exit(1)
+			}
+
+			return nil
+		},
+	}
+
+	cmd.Flags().StringP("meta-data", "m", "", "meta-data content")
+	cmd.Flags().StringP("network-config", "n", "", "network-config content")
+	cmd.Flags().StringP("output", "o", "", "output file name")
+	cmd.Flags().StringP("user-data", "u", "", "user-data content")
+
+	if err := cmd.MarkFlagRequired("output"); err != nil {
+		fmt.Printf("%s", err)
+		os.Exit(1)
+	}
+
+	return cmd
+}
+
+func runCommand(opts *commandOptions) error {
+	size := 10 * 1024 * 1024
+
+	diskFile, err := diskfs.Create(opts.output, int64(size), diskfs.Raw)
+	if err != nil {
+		return err
+	}
+
+	diskFile.LogicalBlocksize = 2048
+
+	diskFileSystem, err := diskFile.CreateFilesystem(disk.FilesystemSpec{
+		Partition: 0,
+		FSType:    filesystem.TypeISO9660,
+	})
+	if err != nil {
+		return err
+	}
+
+	// meta data
+	if opts.metaData != "" {
+		metaDataFile, err := diskFileSystem.OpenFile("meta-data", os.O_CREATE|os.O_RDWR)
+		if err != nil {
+			return err
+		}
+		defer metaDataFile.Close()
+
+		if _, err := metaDataFile.Write([]byte(opts.metaData)); err != nil {
+			return err
+		}
+	}
+
+	// user data
+	if opts.userData != "" {
+		userDataFile, err := diskFileSystem.OpenFile("user-data", os.O_CREATE|os.O_RDWR)
+		if err != nil {
+			return err
+		}
+		defer userDataFile.Close()
+
+		if _, err := userDataFile.Write([]byte(opts.userData)); err != nil {
+			return err
+		}
+	}
+
+	// network config
+	if opts.networkConfig != "" {
+		networkConfigFile, err := diskFileSystem.OpenFile("network-config", os.O_CREATE|os.O_RDWR)
+		if err != nil {
+			return err
+		}
+		defer networkConfigFile.Close()
+
+		if _, err := networkConfigFile.Write([]byte(opts.networkConfig)); err != nil {
+			return err
+		}
+	}
+
+	isoFileSystem, ok := diskFileSystem.(*iso9660.FileSystem)
+	if !ok {
+		return errors.New("invalid file system")
+	}
+
+	return isoFileSystem.Finalize(iso9660.FinalizeOptions{
+		RockRidge:        true,
+		VolumeIdentifier: "cidata",
+	})
+}
